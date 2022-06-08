fac535a8da47f61f776830f4e9ad93535da4d8aa
Sambasivam Krishnamurthy <sambasivamkrishnamurthy@Sambasivams-MacBook-Pro.local>
2022-06-08 13:40:02 +0530

    updates

--- hello-serverless/handler.js
+++ hello-serverless/handler.js
@@ -16,27 +16,3 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-module.exports.add = async (event) => {
-  let {num1,num2} = JSON.parse(event.body)
-  let output = {
-    num1: num1,
-    num2: num2,
-    result: num1 + num2
-  }
-  console.log(output)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(output),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-module.exports.add1 = async (event) => {
-  let {num1,num2} = event
-  return num1 + num2
-  };
-  module.exports.multiply = async (event) => {
-    let {num1,num2} = event
-    return num1 * num2
-    };
